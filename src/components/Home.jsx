3416296cbfe9ce9218865335c4ae8a1783c9aebe
Milyaz Kamil <milyazkamil@gmail.com>
2023-08-26 22:41:08 +0300

    created home component with main header and background image

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="home" id="home">
+      <h1 id="main-title">CONTINUE EXPLORING</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo
+        debitis est autem dicta.
+      </p>
+      <button type="button" className="explore-tours">
+        EXPLORE TOURS
+      </button>
+    </div>
+  )
+}
+export default Home;
