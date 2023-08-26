138f995e818a29393a8f1db094b6aea7b785b8ad
Milyaz Kamil <milyazkamil@gmail.com>
2023-08-26 22:27:29 +0300

    All the components that make up the app were brought together

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import AboutUs from "./AboutUs"
+import FeaturedTours from "./FeaturedTours"
+import Footer from "./Footer"
+import Home from "./Home"
+import Gallery from "./Gallery"
+import Navbar from "./Navbar"
+import OurServices from "./OurServices"
+import Subscribe from "./Subscribe"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <AboutUs />
+      <OurServices />
+      <FeaturedTours />
+      <Subscribe />
+      <Gallery />
+      <Footer />
+    </>
+  )
+}
+export default App;
