d8f78f0e8aff99b4df9879a3a133fa5ce929f54d
Milyaz Kamil <milyazkamil@gmail.com>
2023-08-26 22:23:20 +0300

    created an array containing the information of the tours, services and images

---
+++ src/data.js
@@ -0,0 +1,179 @@
+import tour1 from './assets/images/about.jpeg'
+import tour2 from './assets/images/home.png'
+import tour3 from './assets/images/tour-1.jpeg'
+import tour4 from './assets/images/tour-2.jpeg'
+import tour5 from './assets/images/tour-3.jpeg'
+import tour6 from './assets/images/tour-4.jpeg'
+import tour7 from './assets/images/tour-5.jpeg'
+import tour8 from './assets/images/tour-6.jpeg'
+
+export const tours = [
+  {
+    id: 1,
+    imgUrl: tour3,
+    title: 'Tibet Adventure',
+    date: 'August 26th, 2020',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'China',
+    days: 6,
+    price: 2100,
+  },
+  {
+    id: 2,
+    imgUrl: tour4,
+    title: 'Best Of Java',
+    date: 'October 1th, 2020',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'Indonesia',
+    days: 1,
+    price: 1400,
+  },
+  {
+    id: 3,
+    imgUrl: tour5,
+    title: 'Explore Hong Kong',
+    date: 'September 15th, 2020',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'Hong Kong',
+    days: 8,
+    price: 5000,
+  },
+  {
+    id: 4,
+    imgUrl: tour6,
+    title: 'Kenya Highlights',
+    date: 'December 5th, 2021 ',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'Kenya',
+    days: 7,
+    price: 3300,
+  },
+  {
+    id: 5,
+    imgUrl: tour7,
+    title: 'Tanzania Adventure',
+    date: 'November 5th, 2022',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: ' China',
+    days: 9,
+    price: 2100,
+  },
+  {
+    id: 6,
+    imgUrl: tour8,
+    title: 'Explore India',
+    date: 'March 16th, 2023',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: ' China',
+    days: 3,
+    price: 2100,
+  },
+];
+
+export const images = [
+  {
+    id: 1,
+    imgUrl: tour1,
+  },
+  {
+    id: 2,
+    imgUrl: tour2,
+  },
+  {
+    id: 3,
+    imgUrl: tour3,
+  },
+  {
+    id: 4,
+    imgUrl: tour4,
+  },
+  {
+    id: 5,
+    imgUrl: tour5,
+  },
+  {
+    id: 6,
+    imgUrl: tour6,
+  },
+  {
+    id: 7,
+    imgUrl: tour7,
+  },
+  {
+    id: 8,
+    imgUrl: tour8,
+  },
+];
+
+export const pageLinks = [
+  {
+    id: 1,
+    href: '#home',
+    title: 'Home',
+  },
+  {
+    id: 2,
+    href: '#about',
+    title: 'About',
+  },
+  {
+    id: 3,
+    href: '#services',
+    title: 'Services',
+  },
+  {
+    id: 4,
+    href: '#featured',
+    title: 'Featured',
+  },
+  {
+    id: 5,
+    href: '#gallery',
+    title: 'Gallery',
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: 'https://www.linkedin.com/in/milyazkamil',
+    icon: 'fa-brands fa-linkedin',
+  },
+  {
+    id: 2,
+    href: 'https://www.instagram.com/milyazkamil',
+    icon: 'fa-brands fa-instagram',
+  },
+  {
+    id: 3,
+    href: 'https://www.github.com/milyazkamil',
+    icon: 'fa-brands fa-github',
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fa-solid fa-wallet',
+    title: 'Saving Money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.  Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fa-solid fa-tree',
+    title: 'Endless Hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.  Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fa-solid fa-socks',
+    title: 'Amazing Comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.  Asperiores, officia.',
+  },
+];
